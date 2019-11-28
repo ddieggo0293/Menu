28155001243218c8de3810b0eb45f2a09f5de99b
D&L <D&L@DESKTOP-6DBAOD4>
2019-11-28 00:50:58 -0500

    Caché

--- Calculadora_primer_corte/src/Ejecutar.java
+++ Calculadora_primer_corte/src/Ejecutar.java
@@ -11,46 +11,39 @@ public class Ejecutar {
         int w=1;
         
         while(w==1){
-            
+            //Indicador de Menu-Seleccion de operacion
            int opcion = Integer.parseInt(JOptionPane.showInputDialog("SELECCIONE UNA OPCION\n"+
-                                                                     "1. Operaciones Basicas\n"+
+                                                                     "1. Operaciones basicas\n"+
                                                                      "2. Arreglos\n"+
                                                                      "3. Matrices\n"+
                                                                      "4. Ordenamientos\n"+
                                                                      "5. Listas\n"+
                                                                      "6. Pilas\n"+
                                                                      "7. Salir"));
-           
+     //Indicador de casos
            switch(opcion){
                case 1:
                    obj.opcionA();
                    break;
-                   
                case 2:
                    opcB.opcionB();
                    break;
-                   
                case 3:
                    opcC.opcionC();
                    break;
-                   
                case 4:
                    opcD.opcionD();
                    break;
-                   
                case 5:
                    opcE.opcionE();
                    break;
-                   
                case 6:
                    opcF.opcionF();
                    break;
-                   
                case 7:
                    w=0;
                    JOptionPane.showMessageDialog(null, "SALIENDO");
                    break;
-                   
                default:
                    JOptionPane.showMessageDialog(null, "DIGITE UNA OPCION VALIDA");
         }
